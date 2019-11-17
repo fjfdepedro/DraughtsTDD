c1c77037b1e7bb035b11a84b6c4ccf3b69660165
Francisco José Fernández de Pedro <fjfdepedro@gmail.com>
2019-11-17 16:10:27 +0100

    first step delete session: in controller.java, remove sessions

--- src/main/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/Controller.java
+++ src/main/java/es/urjccode/mastercloudapps/adcs/draughts/controllers/Controller.java
@@ -3,12 +3,10 @@ package es.urjccode.mastercloudapps.adcs.draughts.controllers;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Color;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Coordinate;
 import es.urjccode.mastercloudapps.adcs.draughts.models.Game;
-import es.urjccode.mastercloudapps.adcs.draughts.models.Session;
 import es.urjccode.mastercloudapps.adcs.draughts.models.State;
 
 public abstract class Controller {
 
-	protected Session session;
 	protected Game game;
 	protected State state;
 
